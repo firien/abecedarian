27f8a029d546dba7b1629e23dae67ff47850b775
Patrick Griffin <halifirien@gmail.com>
2018-10-26 19:38:36 -0400

    calculate paths in node, not the browser

--- Cakefile
+++ Cakefile
@@ -1,11 +1,28 @@
 fs = require 'fs'
 path = require 'path'
 coffee = require 'coffeescript'
+svgPath = require 'svg-path-properties'
 
 task('build', 'Build application', (options) ->
   pug = require 'pug'
+  speed = 250
   letters = JSON.parse(fs.readFileSync('letters.json', 'utf8'));
-  html = pug.renderFile('index.pug', letters: letters)
+  letters.forEach((letter) ->
+    delay = 0
+    letter.paths = letter.paths.map((path, i) ->
+      path = { d: path }
+      p = svgPath.svgPathProperties path.d
+      path.length = p.getTotalLength()
+      time = path.length / speed
+      style = "animation-duration:#{time.toFixed(2)}s;"
+      if i > 0
+        style += "animation-delay:#{delay.toFixed(2)}s"
+      delay += time
+      path.style = style
+      path
+    )
+  )
+  html = pug.renderFile('index.pug', letters: letters, pretty: true)
   filename = "docs/index.html"
   dirname = path.dirname(filename)
   if not fs.existsSync(dirname)
--- docs/index.js
+++ docs/index.js
@@ -2,10 +2,9 @@
   document.addEventListener('DOMContentLoaded', function() {
     var callback, observer;
     if (typeof IntersectionObserver !== "undefined" && IntersectionObserver !== null) {
-      // setTimeout( ->
       callback = function(entries, observer) {
         return entries.forEach(function(entry) {
-          if (entry.isIntersecting && entry.intersectionRatio === 1) {
+          if (entry.isIntersecting && Math.abs(entry.intersectionRatio) > 0.96) {
             // github.com/whatwg/html/issues/639
             // history.replaceState(null, null, "##{entry.target.id}")
             return location.hash = entry.target.id;
@@ -15,32 +14,10 @@
       observer = new IntersectionObserver(callback, {
         threshold: 1.0
       });
-      Array.prototype.slice.call(document.querySelectorAll('div[id^=letter]')).forEach(function(element) {
+      return Array.prototype.slice.call(document.querySelectorAll('div[id^=letter]')).forEach(function(element) {
         return observer.observe(element);
       });
     }
-    // , 300)
-    return window.requestAnimationFrame(function() {
-      var speed;
-      speed = 250; //pixels/sec
-      return Array.prototype.slice.call(document.querySelectorAll('svg')).forEach(function(svg) {
-        var delay;
-        delay = 0;
-        return Array.prototype.slice.call(svg.querySelectorAll('path')).reverse().forEach(function(path, i) {
-          var css, length, time;
-          length = path.getTotalLength();
-          path.setAttribute('stroke-dasharray', length);
-          path.setAttribute('stroke-dashoffset', length);
-          time = length / speed;
-          css = `animation-duration:${time.toFixed(2)}s;`;
-          if (i > 0) {
-            css += `animation-delay:${delay.toFixed(2)}s`;
-          }
-          delay += time;
-          return path.setAttribute('style', css);
-        });
-      });
-    });
   });
 
 }).call(this);

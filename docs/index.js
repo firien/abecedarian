c1ffb3109ae7294e707323f493c4aad4b0960d1b
Patrick Griffin <halifirien@gmail.com>
2018-11-14 21:00:24 -0500

    fix for Windows

--- docs/index.js
+++ docs/index.js
@@ -51,7 +51,7 @@
       document.body.classList.add('draw');
       callback = function(entries, observer) {
         return entries.forEach(function(entry) {
-          if (entry.isIntersecting && Math.abs(entry.intersectionRatio) > 0.96) {
+          if (entry.isIntersecting && Math.abs(entry.intersectionRatio) > 0.9) {
             // github.com/whatwg/html/issues/639
             // history.replaceState(null, null, "##{entry.target.id}")
             return location.hash = entry.target.id;
@@ -59,7 +59,7 @@
         });
       };
       observer = new IntersectionObserver(callback, {
-        threshold: 1.0
+        threshold: 0.9
       });
       return Array.prototype.slice.call(document.querySelectorAll('div[id^=letter]')).forEach(function(element) {
         return observer.observe(element);
--- index.coffee
+++ index.coffee
@@ -37,12 +37,12 @@ document.addEventListener('DOMContentLoaded', ->
     document.body.classList.add('draw')
     callback = (entries, observer) ->
       entries.forEach((entry) ->
-        if entry.isIntersecting && Math.abs(entry.intersectionRatio) > 0.96
+        if entry.isIntersecting && Math.abs(entry.intersectionRatio) > 0.9
           # github.com/whatwg/html/issues/639
           # history.replaceState(null, null, "##{entry.target.id}")
           location.hash = entry.target.id
       )
-    observer = new IntersectionObserver(callback, threshold: 1.0)
+    observer = new IntersectionObserver(callback, threshold: 0.9)
     Array::slice.call(document.querySelectorAll('div[id^=letter]')).forEach((element) ->
       observer.observe(element)
     )
